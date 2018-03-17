14466bd5ca77be0ff62004e2d1206f4bb6a1d749
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-03-17 11:33:28 +0100

    Inintial settings of the backend

---
+++ src/main/java/org/java/smartaccount/SmartAccountAppApplication.java
@@ -0,0 +1,12 @@
+package org.java.smartaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAccountAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAccountAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/java/smartaccount/SmartAccountAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.java.smartaccount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartAccountAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
